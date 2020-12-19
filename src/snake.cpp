43da31e0de6491f6341a74490328e7b0863023ec
maciekbartczak <maciekbartczak17@gmail.com>
2020-12-19 18:08:30 +0100

    Pause and help can now be toggled, level is displayed.

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,15 +1,24 @@
 #include "snake.h"
 #include "screen.h"
+#include <cstring>
 
 CSnake::CSnake(CRect r, char _c /*=' '*/):
   CFramedWindow(r, _c)
 {
-
+  level = 1;
+  isPaused = true;
+  displayHelp = true;
 }
 
 void CSnake::paint() {
-  CFramedWindow::paint();
-  paintHelp();
+    CFramedWindow::paint();
+    paintLevel();
+    if(displayHelp){
+      paintHelp();
+    }
+    if(isPaused){
+      paintPause();
+    }
 }
 void CSnake::paintHelp() {
   gotoyx(geom.topleft.y + 3, geom.topleft.x + 3);
@@ -22,4 +31,29 @@ void CSnake::paintHelp() {
   printl("%s", "arrows - move snake (play mode)");
   gotoyx(geom.topleft.y + 7, geom.topleft.x + 12);
   printl("%s","move window (pause mode)");
+}
+bool CSnake::handleEvent(int c) {
+  if(isPaused){
+      if(CFramedWindow::handleEvent(c)){
+        return true;
+      }
+  }
+  if( c == 'h'){
+    displayHelp = !displayHelp;
+  }
+  if( c == 'p' ){
+    isPaused = !isPaused;
+  }
+  return true;
+}
+
+void CSnake::paintPause(){
+  const char caption[] = "GAME PAUSED";
+  gotoyx( geom.topleft.y + (geom.size.y / 2), geom.topleft.x + ((geom.size.x - std::strlen(caption)) / 2) );
+  printl("%s", caption);
+}
+
+void CSnake::paintLevel() {
+  gotoyx(geom.topleft.y - 1, geom.topleft.x);
+  printl("| LEVEL %d |",level);
 }
--- src/snake.h
+++ src/snake.h
@@ -2,15 +2,21 @@
 #define __SNAKE_H__
 
 #include "winsys.h"
+#include <ncurses.h>
 
 class CSnake:public CFramedWindow
 {
 public:
   CSnake(CRect r, char _c = ' ');
   void paint();
+  bool handleEvent(int c);
 private:
   unsigned int level;
+  bool displayHelp;
+  bool isPaused;
   void paintHelp();
+  void paintPause();
+  void paintLevel();
 };
 
 #endif
